ec21e3cb6a682167dae67c3ab871ebb55db17c33
Ciro <cirodipaolo15@gmail.com>
2025-10-28 22:27:48 -0300

    cleaned code

--- src/app/pages/auth/auth-modal-component/auth-modal-component.ts
+++ src/app/pages/auth/auth-modal-component/auth-modal-component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { LoginForm } from '../login-form/login-form';
-import { RegisterForm } from '../register-form/register-form/register-form';
+import { RegisterForm } from '../register-form/register-form';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -10,8 +10,8 @@ import { MatTabsModule } from '@angular/material/tabs';
 @Component({
   selector: 'app-auth-modal-component',
   imports: [ LoginForm, RegisterForm, MatDialogModule,
-    MatTabsModule,   // <-- ¡AÑADIR AQUÍ!
-    MatIconModule,   // <-- ¡AÑADIR AQUÍ!
+    MatTabsModule,   
+    MatIconModule,   
     MatButtonModule, ],
   templateUrl: './auth-modal-component.html',
   styleUrl: './auth-modal-component.css',
