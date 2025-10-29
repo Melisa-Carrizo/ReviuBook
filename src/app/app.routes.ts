35a96340e7caac45f68a8fbd3a872cc4fcf0d5b5
Melisa Carrizo <melisacarrizo5@gmail.com>
2025-10-29 01:32:14 -0300

    add paths to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { BookSheetComponent } from './pages/booksheet/book-sheet/book-sheet';
+import { HomeComponent } from './pages/home/home-component/home-component';
 
 export const routes: Routes = [
+    {path:'', redirectTo:'home', pathMatch:'full'},
+    {path:'home', component:HomeComponent},
     {path: 'libro/:id', component: BookSheetComponent}
 ];
