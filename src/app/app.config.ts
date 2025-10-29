17fa3178272e12ca63da3ac9f885ff06511c82c0
Melisa Carrizo <melisacarrizo5@gmail.com>
2025-10-29 01:31:31 -0300

    add BookService to app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { BookService } from './core/services/book-service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    {provide: BookService}
   ]
 };
