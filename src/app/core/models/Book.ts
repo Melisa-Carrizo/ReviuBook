a6d5ad4a3480510a976c0559d1ddd5bb1b01e54a
Melisa Carrizo <melisacarrizo5@gmail.com>
2025-10-29 01:39:51 -0300

    refactor: update Book model to meet requirements

--- src/app/core/models/Book.ts
+++ src/app/core/models/Book.ts
@@ -1,14 +1,12 @@
-import { Review } from "./Review"
-
 export interface Book {
     id: number,
     category: string,
     description: string,
     releaseDate: Date,
     status: boolean,
+    urlImage:string,
     ISBN: string,
     title: string
     author: string,
     publishinHouse: string,
-    reviews: Review[]
 }
