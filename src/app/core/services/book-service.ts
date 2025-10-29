ea78be16ab4aab9f9b3c6cd0aae3e3def82e302d
Melisa Carrizo <melisacarrizo5@gmail.com>
2025-10-29 01:40:39 -0300

    chore: add method getAllActiveBooks for listing

--- src/app/core/services/book-service.ts
+++ src/app/core/services/book-service.ts
@@ -2,12 +2,17 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Book } from '../models/Book';
 import { BookSheet } from '../models/BookSheet';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
   private http = inject(HttpClient);
   baseUrl = 'http://localhost:8080/libros';
+
+  getAllActiveBooks():Observable<Book[]>{
+    return this.http.get<Book[]>(`${this.baseUrl}/all/active`);
+  }
 
   getBookSheetById(id: string) {
     return this.http.get<BookSheet>(`${this.baseUrl}/bookSheet/${id}`);
