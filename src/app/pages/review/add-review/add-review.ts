52adad6cd9126e5fc2f92506d1c682c86eca6a90
Castro Lautaro <castrolautaro235@gmail.com>
2025-10-30 18:53:22 -0300

    Creando form group en AddReview component

--- src/app/pages/review/add-review/add-review.ts
+++ src/app/pages/review/add-review/add-review.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-review',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-review.html',
   styleUrl: './add-review.css',
 })
 export class AddReview {
+  private fb = inject(NonNullableFormBuilder);
 
+  form = this.fb.group(
+    {
+      content: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(250)]],
+      rating: [0, [Validators.required, Validators.min(1), Validators.max(5)]]
+    }
+  );
+
+  
 }
