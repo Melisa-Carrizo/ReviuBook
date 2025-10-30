9ff099804acd1ed5116e74af34bd7fc7e6cb98db
Castro Lautaro <castrolautaro235@gmail.com>
2025-10-30 15:05:14 -0300

    Creando componente ModifyReview

---
+++ src/app/pages/review/modify-review/modify-review.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-review',
+  imports: [],
+  templateUrl: './modify-review.html',
+  styleUrl: './modify-review.css',
+})
+export class ModifyReview {
+
+}
