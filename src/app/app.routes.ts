98583441f345239bce0e7689d3abab2517edbefb
Ciro <cirodipaolo15@gmail.com>
2025-10-30 14:09:17 -0300

    created page userProfile and guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { BookSheetComponent } from './pages/booksheet/book-sheet/book-sheet';
 import { HomeComponent } from './pages/home/home-component/home-component';
+import { UserProfile } from './pages/userProfile/user-profile/user-profile';
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
     {path:'', redirectTo:'home', pathMatch:'full'},
     {path:'home', component:HomeComponent},
-    {path: 'libro/:id', component: BookSheetComponent}
+    {path: 'libro/:id', component: BookSheetComponent},
+    {path: 'profile', component: UserProfile, canActivate: [authGuard]}
 ];
