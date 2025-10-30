76ccd9778fed1bdceefd6dcf59a9b3d944c7df25
Ciro <cirodipaolo15@gmail.com>
2025-10-30 14:05:48 -0300

    modification the manipulation of token

--- src/app/core/services/auth-service.ts
+++ src/app/core/services/auth-service.ts
@@ -7,6 +7,7 @@ import { Token } from '../models/token';
 import { User } from '../models/User';
 import { jwtDecode } from 'jwt-decode';
 import { UserService } from './user-service';
+import { authGuard } from '../guards/auth-guard';
 
 @Injectable({
   providedIn: 'root'
@@ -16,14 +17,17 @@ export class ApiConnectionAuth {
   private apiUrl = 'http://localhost:8080/auth';
   private http = inject(HttpClient);
   private userService = inject(UserService);
-  currentUser = signal<User | null>(this.getUserFromStorage());
-  isLoggedIn = computed(() => this.currentUser() !== null);
+  private authToken = signal<string | null>(localStorage.getItem('authToken'));
 
+  currentUser = signal<User | null>(this.getUserFromStorage());
+  isLoggedIn = computed(() => !!this.authToken());
+  
   login(credentials: LoginRequest): Observable<User> { 
     return this.http.post<Token>(`${this.apiUrl}/login`, credentials).pipe(
       tap(response => {
         if (response && response.token) {
           localStorage.setItem('authToken', response.token);
+          this.authToken.set(response.token);
         }
       }),
       switchMap(() => this.userService.getUserProfileByToken()),
@@ -32,7 +36,7 @@ export class ApiConnectionAuth {
           this.currentUser.set(user);
       })
     );
-}
+  }
 
   register(data: Partial<RegisterRequest>): Observable<string> {
     return this.http.post(`${this.apiUrl}/register`, data, {
@@ -43,6 +47,7 @@ export class ApiConnectionAuth {
   logout(): void {
     localStorage.removeItem('authToken');
     localStorage.removeItem('currentUser');
+    this.authToken.set(null);
     this.currentUser.set(null);
   }
 
@@ -51,16 +56,4 @@ export class ApiConnectionAuth {
     return userJson ? (JSON.parse(userJson) as User) : null;
   }
 
-  private decodeToken(token: string): User {
-    try {
-      const decoded: any = jwtDecode(token);
-      return {
-        username: decoded.username || decoded.sub || 'Usuario',
-        email: decoded.email || ''
-      };
-    } catch (error) {
-      console.error('Error decodificando el token:', error);
-      return { username: 'Usuario', email: 'error@mail.com' };
-    }
-  }
 }
