36fb914ea76ee32477423c442bdc5e8bf4f4882f
Castro Lautaro <castrolautaro235@gmail.com>
2025-11-01 15:24:47 -0300

    Agregando logica para la busqueda de libros en Home.ts y Home.html

--- src/app/pages/home/home-component/home-component.ts
+++ src/app/pages/home/home-component/home-component.ts
@@ -1,10 +1,11 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { BookService } from '../../../core/services/book-service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { BookCardComponent } from "../book-card-component/book-card-component";
 import { catchError, of } from 'rxjs';
 import { Book } from '../../../core/models/Book';
 import { SnackbarService } from '../../../core/services/snackbar-service';
+import { SearchService } from '../../../core/services/search-service';
 
 @Component({
   selector: 'app-home-component',
@@ -14,6 +15,7 @@ import { SnackbarService } from '../../../core/services/snackbar-service';
 })
 export class HomeComponent {
   private _bookService = inject(BookService);
+  private _searchService = inject(SearchService);
   private snackBar = inject(SnackbarService);
   bookList = toSignal(
     this._bookService.getAllActiveBooks().pipe(
@@ -23,6 +25,21 @@ export class HomeComponent {
     ),
     { initialValue: [] as Book[] }
   );
+
+  //Libros filtrados segun el termino de busqueda
+  filteredBooks = computed(() => {
+    const searchTerm = this._searchService.searchTerm().toLowerCase().trim();
+    const allBooks = this.bookList();
+    // si no hay termino de busqueda, se muestra la lista original
+    if (!searchTerm) {
+      return allBooks
+    }
+    // se filtran los libros segun su titulo o autor
+    return allBooks.filter(b => 
+      b.title.toLocaleLowerCase().includes(searchTerm) ||
+      b.author.toLocaleLowerCase().includes(searchTerm)
+    );
+  })
 
   sidebarOpen = signal(false)
   toggleSidebar() { this.sidebarOpen.update(p => !p) }
