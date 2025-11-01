285b94fda1183d0359c09134519f934e091b2ddc
Castro Lautaro <castrolautaro235@gmail.com>
2025-11-01 15:02:59 -0300

    Agregando metodo para setear termino de busqueda en SearchService

--- src/app/core/services/search-service.ts
+++ src/app/core/services/search-service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
-  
+  public searchTerm = signal<string>(''); // guarda el termino de busqueda
+
+  // actualiza el termino de busqueda
+  setSearchTerm(term: string) {
+    this.searchTerm.set(term);
+  }
 }
