c19b29f8e317d3a189ad9dcebf744728804e7bdd
Castro Lautaro <castrolautaro235@gmail.com>
2025-11-06 15:50:53 -0300

    Creando ManageReviewsComponent

---
+++ src/app/pages/admin/manage-reviews-component/manage-reviews-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-reviews-component',
+  imports: [],
+  templateUrl: './manage-reviews-component.html',
+  styleUrl: './manage-reviews-component.css',
+})
+export class ManageReviewsComponent {
+
+}
