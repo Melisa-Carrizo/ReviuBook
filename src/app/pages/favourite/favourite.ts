227a0e126ef9e37d6d365fabcf1ba5c72a5c7080
Castro Lautaro <castrolautaro235@gmail.com>
2026-01-07 12:29:54 -0300

    feature(Favourite): add method to delete a book from the list.

--- src/app/pages/favourite/favourite.ts
+++ src/app/pages/favourite/favourite.ts
@@ -2,6 +2,7 @@ import { Component, effect, inject, signal, WritableSignal } from '@angular/core
 import { BookStageService } from '../../core/services/book-stage';
 import { Router } from '@angular/router';
 import { BookStage } from '../../core/models/BookStage';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-favourite',
@@ -23,8 +24,22 @@ export class Favourite {
           next: (data) => this.favouriteBooks.set(data),
           error: e => console.error("Error al obtener la lista de favoritos: ", e.message)
         }
-      )
+      );
     })
+  }
+
+  // Eliminar el libro de favoritos y actualiza el signal
+  deleteFromFavourite(idBookStage: number) {
+    this._bookStage.deleteBookStage(idBookStage).subscribe(
+      {
+        next: (data) => {
+          this.favouriteBooks.update(
+            b => b.filter(b => b.id !== idBookStage)
+          );
+        },
+        error: e => console.error("Error al eliminar el libro de la lista: ", e.message)
+      }
+    );
   }
 
 }
