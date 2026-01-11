0d3158bed64dc08b91a8e6986890417a0b54318d
Castro Lautaro <castrolautaro235@gmail.com>
2026-01-11 12:32:12 -0300

    feature(ReviewPanel): implement attributes and methods to get all the reviews of a book.

--- src/app/pages/admin/review-panel/review-panel.ts
+++ src/app/pages/admin/review-panel/review-panel.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, signal, WritableSignal } from '@angular/core';
+import { ReviewService } from '../../../core/services/review-service';
+import { Review } from '../../../core/models/Review';
+import { Book } from '../../../core/models/Book';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { BookService } from '../../../core/services/book-service';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-review-panel',
@@ -7,5 +14,25 @@ import { Component } from '@angular/core';
   styleUrl: './review-panel.css',
 })
 export class ReviewPanel {
+  private _reviewService = inject(ReviewService);
+  private _bookService = inject(BookService);
+  private _route = inject(ActivatedRoute);
+  selectedBook = toSignal(
+    this._route.paramMap.pipe(
+      switchMap(params => this._bookService.getBookById(params.get('id')!))
+    ),
+    {initialValue : undefined}
+  );
+  reviews: WritableSignal<Review[] | undefined> = signal([]);
+  
 
+  constructor() {
+    const bookId = String(this.selectedBook()?.id)
+    this._reviewService.getAllByBookId(bookId).subscribe({
+      next: (data) => {
+        this.reviews.set(data)
+      },
+      error: err => console.error("No se pudieron obtener las reseñas del libro: ", err.message)
+    });
+  }
 }
