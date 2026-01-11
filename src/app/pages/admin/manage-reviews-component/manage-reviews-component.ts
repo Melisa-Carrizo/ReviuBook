880a39ae82286657580498cebcc0b793750ab581
Castro Lautaro <castrolautaro235@gmail.com>
2026-01-10 22:23:31 -0300

    feature(ManageReviews): add attributes to get all the books.

--- src/app/pages/admin/manage-reviews-component/manage-reviews-component.ts
+++ src/app/pages/admin/manage-reviews-component/manage-reviews-component.ts
@@ -7,6 +7,7 @@ import { Review } from '../../../core/models/Review';
 import { ReviewService } from '../../../core/services/review-service';
 import { Book } from '../../../core/models/Book';
 import { ReviewItem } from '../../booksheet/review-item/review-item';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-manage-reviews-component',
@@ -15,6 +16,10 @@ import { ReviewItem } from '../../booksheet/review-item/review-item';
   styleUrl: './manage-reviews-component.css',
 })
 export class ManageReviewsComponent {
- 
+  private _bookService = inject(BookService);
+  private _router = inject(Router);
+  books: Book[] = [];
+  isLoading: boolean = true;
+  hasBooks: boolean = false;
 
 }
