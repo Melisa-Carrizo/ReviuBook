1f1bc032aae81c0a1945ac970e27b9775eee7542
Castro Lautaro <castrolautaro235@gmail.com>
2026-01-11 12:15:25 -0300

    feature(ReviewPanel): add component to manage the reviews of a book.

---
+++ src/app/pages/admin/review-panel/review-panel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-panel',
+  imports: [],
+  templateUrl: './review-panel.html',
+  styleUrl: './review-panel.css',
+})
+export class ReviewPanel {
+
+}
