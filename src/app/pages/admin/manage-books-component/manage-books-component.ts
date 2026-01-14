b8eb31632a2f406b3c7a4b4519bab3dd823d4425
Melisa Carrizo <melisacarrizo5@gmail.com>
2026-01-14 15:02:36 -0300

    fix(pagination): adapt component to support pagination

--- src/app/pages/admin/manage-books-component/manage-books-component.ts
+++ src/app/pages/admin/manage-books-component/manage-books-component.ts
@@ -1,43 +1,65 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import Swal, { SweetAlertResult } from 'sweetalert2';
 import { Book } from '../../../core/models/Book';
 import { BookPreviewComponent } from "../book-preview-component/book-preview-component";
 import { BookService } from '../../../core/services/book-service';
 import { Router } from '@angular/router';
 import { SnackbarService } from '../../../core/services/snackbar-service';
+import { Page } from '../../../core/models/Page';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { catchError, of, switchMap } from 'rxjs';
+import { PaginationBar } from "../../../shared/components/pagination-bar/pagination-bar";
 
 @Component({
     selector: 'app-manage-books-component',
-    imports: [BookPreviewComponent],
+    imports: [BookPreviewComponent, PaginationBar],
     templateUrl: './manage-books-component.html',
     styleUrl: './manage-books-component.css',
 })
-export class ManageBooksComponent implements OnInit{
+export class ManageBooksComponent {
     private bookService = inject(BookService);
     private router = inject(Router);
     snackBar = inject(SnackbarService);
-    public books: Book[] = [];
-    isLoading: boolean = true;
-    hasBooks: boolean = false;
-
-    ngOnInit(): void {
-        this.loadBooks();
-    }
-
-    loadBooks(): void {
-        this.isLoading = true;
-        this.bookService.getAll().subscribe({
-            next: (data) => {
-                this.books = data;
-                this.isLoading = false;
-                this.hasBooks = this.books && this.books.length > 0;
-            },
-            error: (err) => {
-                console.error('Error al cargar libros:', err);
-                this.isLoading = false;
+    currentPage = signal(0);
+    booksPage = toSignal(
+        toObservable(this.currentPage).pipe(
+            switchMap(page =>
+            this.bookService.getAll(page).pipe(
+                catchError(err => {
+                console.error(err);
                 this.snackBar.openErrorSnackBar('Error al cargar la lista de libros.');
-            }
-        });
+                return of({
+                    content: [],
+                    page: { number: 0, totalPages: 0, totalElements: 0, size: 0 }
+                });
+                })
+            )
+            )
+        ),
+        { initialValue: { content: [], page: { number: 0, totalPages: 0, totalElements: 0, size: 0 } } }
+    );
+    isLoading = computed(() => {
+        return this.booksPage().content.length === 0
+        && this.booksPage().page.totalElements === 0;
+    });
+
+    hasBooks = computed(() => {
+        return this.booksPage().content.length > 0;
+    });
+    
+    pageMetaData = computed(() => {
+        const page = this.booksPage().page;
+
+        return {
+            number: page.number,
+            totalPages: page.totalPages,
+            totalElements: page.totalElements,
+            size: page.size
+        };
+    });
+
+    onPageChange(page: number) {
+        this.currentPage.set(page);
     }
 
     goToAddBook(){
@@ -64,13 +86,7 @@ export class ManageBooksComponent implements OnInit{
                     {
                         next: () => {
                             this.snackBar.openSuccessSnackBar('¡Libro borrado!');
-
-                            // 🚨 ACTUALIZACIÓN CLAVE: Actualización local del estado
-                            const index = this.books.findIndex(b => b.id === book.id);
-                            if (index !== -1) {
-                                // Crea una copia del objeto para activar la detección de cambios de Angular
-                                this.books[index] = { ...this.books[index], status: false };
-                            }
+                            this.currentPage.set(this.currentPage()); // fuerza recarga
                         },
                         error: (err) => {
                             let errorMessage = 'Error desconocido al borrar el libro.';
