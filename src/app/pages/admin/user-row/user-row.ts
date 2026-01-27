2eb396c8c07cfc8ace720c2bdf7245e139bebf33
Melisa Carrizo <melisacarrizo5@gmail.com>
2026-01-27 00:28:32 -0300

    refactor: modify component to support deleting/activating

--- src/app/pages/admin/user-row/user-row.ts
+++ src/app/pages/admin/user-row/user-row.ts
@@ -1,4 +1,4 @@
-import { Component, input, inject } from '@angular/core';
+import { Component, input, inject, output } from '@angular/core';
 import { User } from '../../../core/models/User';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { UserProfileModalComponent } from '../../../shared/components/user-profile-modal/user-profile-modal';
@@ -11,19 +11,28 @@ import { UserProfileModalComponent } from '../../../shared/components/user-profi
 })
 export class UserRow {
   user = input.required<User>();
-
   private dialog = inject(MatDialog);
+  statusChanged = output<{ email: string; status: boolean }>();
 
   openEditModal() {
     const u = this.user();
     const userId = (u as any)?.id;
-    this.dialog.open(UserProfileModalComponent, {
+    const dialogRef = this.dialog.open(UserProfileModalComponent, {
       maxWidth: '900px',
       width: '100%',
       maxHeight: '90vh',
       autoFocus: false,
       hasBackdrop: true,
-      data: { email: u.email, username: u.username, id: userId }
+      data: {
+        email: u.email,
+        username: u.username,
+        id: userId
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (!result) return;
+      this.statusChanged.emit(result);
     });
   }
 }
